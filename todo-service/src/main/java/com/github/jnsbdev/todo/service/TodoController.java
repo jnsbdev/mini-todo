ef3787dabdbcfb1c3d1f05fb7ee25d4f884c9e65
Jonas <jonasbucher95@gmail.com>
2025-08-14 11:05:41 +0200

    feature: todo microservice
    - add todo repo, service and controller to add todos on /todos endpoint
    - defined security config with jwt decoder and validations
    - change application port to 8081
    - add basic entity and DTOs
    - add necessary dependencies

---
+++ todo-service/src/main/java/com/github/jnsbdev/todo/service/TodoController.java
@@ -0,0 +1,27 @@
+package com.github.jnsbdev.todo.service;
+
+import com.github.jnsbdev.todo.model.TodoRequest;
+import com.github.jnsbdev.todo.model.TodoResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/todos")
+@RequiredArgsConstructor
+public class TodoController {
+
+    private final TodoService todoService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public TodoResponse save(
+            @AuthenticationPrincipal Jwt jwt,
+            @RequestBody TodoRequest request
+    ) {
+        return todoService.save(jwt.getSubject(), request);
+    }
+
+}
